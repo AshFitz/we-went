5edd5f04c87bbd34b516acc0ece30bf7bb8afafa
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-12 20:45:46 +0000

    Add post & comment to admin panel, to manage posts

--- socialblog/admin.py
+++ socialblog/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+
+class CommentItemInline(admin.TabularInline):
+    model = Comment
+    raw_id_fields = ['post']
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'rating', 'created_on')
+    search_fields = ['title', 'description', 'rating']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on', 'rating')
     summernote_fileds = ('description')
+    inlines = [CommentItemInline]
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
 
-
+    list_display = ('name', 'body', 'post', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('name', 'email', 'body')
