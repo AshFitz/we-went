0f979a1772a62951f3344e27a642995fab2a016d
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-13 17:48:31 +0000

    Add root url

--- wewent/urls.py
+++ wewent/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("socialblog.urls"), name="social-blog-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
