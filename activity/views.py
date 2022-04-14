03e3784fce2b259e3c5456770555d95222a9e98e
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-14 21:47:39 +0000

    Update URL, add block content to base

--- activity/views.py
+++ activity/views.py
@@ -6,7 +6,7 @@ from .models import Post
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.order_by('-created_on')
-    template_name = 'base.html'
+    template_name = 'index.html'
     
 
 
