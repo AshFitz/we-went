4effe920704ba3a258bf03da59eb6c563abfd13f
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-15 19:08:00 +0000

    Mock up view page

--- activity/views.py
+++ activity/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Post
-
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Post, Comment
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.order_by('-created_on')
     template_name = 'index.html'
-    
 
 
+# class PostDetails(View):
+
+#     def get(self, request, *args, **kwargs):
+#         queryset = Post.objects.all()
+#         # post = queryset.
+#         # comments = post.comments.order_by('-created_on')
+
+#         return render(
+#             request,
+#             "index.html",
+#             {
+#                 # "post" : post,
+#                 # "comments" : comments,
+#             },
+#         )
