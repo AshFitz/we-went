618a9eac6f6d51686c7e974c265ca8ba08182b2a
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-16 20:57:14 +0000

    Add account urls

--- wewent/urls.py
+++ wewent/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("activity.urls"), name="activity-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
