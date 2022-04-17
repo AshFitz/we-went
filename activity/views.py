833f517da79b321e5f1d6c1c2b8c8d92ef37f024
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-17 12:41:03 +0000

    Overide context data to display input form

--- activity/views.py
+++ activity/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from .models import Post, Comment
+from .models import Post
+from .forms import CommentForm
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.order_by('-created_on')
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        context["comment_form"] = CommentForm()
+        return context
 
-# class PostDetails(View):
-
-#     def get(self, request, *args, **kwargs):
-#         queryset = Post.objects.all()
-#         # post = queryset.
-#         # comments = post.comments.order_by('-created_on')
-
-#         return render(
-#             request,
-#             "index.html",
-#             {
-#                 # "post" : post,
-#                 # "comments" : comments,
-#             },
-#         )
+    
