0662d8404a59d124de9f9b32d16c6e292299c751
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-18 18:22:40 +0000

    Change order or URL paths

--- wewent/urls.py
+++ wewent/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("activity.urls"), name="activity-urls"),
     path('about/', include("about.urls"), name="about-urls"),
+    path("", include("activity.urls"), name="activity-urls"),
     path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
 ]
