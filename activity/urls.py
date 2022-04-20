324a6eba336fac42a977ef09e9a72098d0866ddb
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-20 23:03:34 +0000

    added new url for new like function / ajax request

--- activity/urls.py
+++ activity/urls.py
@@ -2,6 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('like/', views.like, name='like'),
     path("", views.PostList.as_view(), name="home",),
-    path('<slug:slug>/', views.Comment.as_view(), name='comment')
+    path('<slug:slug>/', views.Comment.as_view(), name='comment'),  
+    #path('like/<slug:slug>/', views.PostLike.as_view(), name='post_like'),
 ]
