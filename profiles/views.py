664b3f4c59879a3e391178e495ef338fb03f8d62
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-24 18:41:58 +0000

    Create user profile function which gets the user from db

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import UserProfile
+from activity.models import Post
 
 # Create your views here.
+
+
+#Profile function
+
+def profile(request):
+    #profile = UserProfile.objects.create(user=request.user)
+    profile = get_object_or_404(UserProfile, user=request.user)
+   
+   
+    template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+        # 'posts': posts
+    }
+
+    return render(request, template, context)
+
+
+#Post history
+
+
+#likes history
