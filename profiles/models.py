ebe04f2cd301b05eeb4982a7990a182cec20b8dd
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-24 18:41:34 +0000

    create user profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,15 @@
-from django.db import models, Post
+from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class UserProfile(models.Model):
     """
     A user profile model to link posts and likes
     """
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-   
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
 
+    def __str__(self):
+        return self.user.username
 
