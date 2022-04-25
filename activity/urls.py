d94bc6556e016ede4bc8bff85370aa266e3d36f9
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-25 21:53:03 +0000

    add edit_comment url

--- activity/urls.py
+++ activity/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("add/", views.add_post, name="add_post"),
     path('edit/<int:post_id>', views.edit_post, name='edit_post'),
     path('delete/<int:post_id>', views.delete_post, name='delete_post'),
+    path('edit_comment/<int:post_id>', views.edit_comment, name='edit_comment'),
     #path('post/<int:pk>', views.add_post, name="post_detail"),
     path("", views.PostList.as_view(), name="home"),
     path('comment/<int:post_id>/', views.Comment.as_view(), name='comment'),
