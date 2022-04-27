9a285b2e18c62408cf9a59284abbf11a56e3d307
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-04-27 19:06:40 +0000

    add click functions to add & remove class to profile nav

---
+++ static/js/main.js
@@ -0,0 +1,31 @@
+const postPanel = $('#post-panel');
+const likesPanel = $('#likes-panel');
+const logOutPanel = $('#logout-panel');
+
+//click function to handle the active class for profile navigation
+$('li').click(function() {
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+});
+
+//onclick events to handle which panel is showing per click
+$('#post-nav-button').click(function(){
+    postPanel.show();
+    likesPanel.hide();
+    logOutPanel.hide();
+    return false;
+});
+
+$('#likes-nav-button').click(function(){
+    postPanel.hide();
+    logOutPanel.hide();
+    likesPanel.show();
+    return false;
+});
+
+$('#logout-nav-button').click(function(){
+    postPanel.hide();
+    likesPanel.hide();
+    logOutPanel.show();
+    return false;
+});
