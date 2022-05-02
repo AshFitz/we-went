05d56de135151253e2e0bde7e6332338fe1e8cee
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-05-02 07:42:09 +0000

    add imports

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from activity.models import Post, Comment
 from .models import UserProfile
 
